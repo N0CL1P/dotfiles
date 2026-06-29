e6558ba9eebf358c6632de681ff6f0e77b442c09
N0CL1P <lolakforgame1337@gmail.com>
2026-06-29 13:03:48 +0500

    update

--- configuration.nix
+++ configuration.nix
@@ -124,7 +124,6 @@
     obs-studio = {
       enable = true;
       plugins = [
-        pkgs.obs-studio-plugins.obs-vkcapture
         pkgs.obs-studio-plugins.obs-pipewire-audio-capture
       ];
     };
